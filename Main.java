8b4e84f8c737dbd24c0c7a2d43969d09222fc80e
ArtemisGate <scott_tat@yahoo.com>
2019-10-14 20:45:10 -0500

    Intial Commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		//EarthQuakeClient quake1 = new EarthQuakeClient();
+		ClosestQuakes quake2 = new ClosestQuakes();
+		//quake1.bigQuakes();
+		quake2.findClosestQuakes();
+		
+		//quake1.closeToMe();
+		
+		//quake1.quakeOfDepth();
+		
+		
+
+	}
+
+}
